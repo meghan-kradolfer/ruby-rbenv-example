a431ab9e2d7578f518a6f46e4000110df291afe4
Meghan <meghan.kradolfer@gmail.com>
2023-10-10 11:03:54 +1300

    Update sample_spec.rb

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,6 +1,12 @@
 require "spec_helper"
 RSpec.describe "A sample test" do
   it "runs aok" do
-    expect(42).to eql(43)
+    expect(42).to eql(42)
+  end
+  it "doesn't work" do
+    expect(42).to eql(41)
+  end
+  it "fails" do
+    expect(42).to eql(39)
   end
 end
