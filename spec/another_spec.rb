18bcb8fd100ac4d3dc122acd88cd702f5b25af03
Meghan Kradolfer <meghan@buildkite.com>
2023-10-13 09:46:00 +1300

    New spec

---
+++ spec/another_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+RSpec.describe "So many failing tests" do
+  it "should fail when the data type is incorrect" do
+    string_value = "42"
+    expect(string_value).to be_a(Integer)
+  end
+
+  it "should fail when expectation is not met" do
+    value = 42
+    expect(value).to be < 40 # Value is not less than 40
+  end
+
+  it "should fail when using an uninitialized variable" do
+    uninitialized_variable # Attempting to use an undefined variable
+    expect(true).to be(true)
+  end
+end
--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -9,10 +9,7 @@ RSpec.describe "A sample test" do
   it "fails" do
     expect(42).to eql(39)
   end
-  it "should fail when no exception is raised" do
-    expect { 1 / 0 }.to raise_error(ZeroDivisionError)
-  end
-    it "should fail when values are equal" do
+  it "should fail when values are equal" do
     expect(3 * 4).not_to eq(12)
   end
 end
