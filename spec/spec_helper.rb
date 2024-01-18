4e738bf845709277dd16c1b5b557f73d03a1356d
Meghan <meghan.kradolfer@gmail.com>
2024-01-19 10:18:38 +1300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ require "buildkite/test_collector"
 
 Buildkite::TestCollector.configure(
   hook: :rspec,
-  token: "MfT3gHLW9DhZuxpH7fnWd6ZL",
-  url: "http://analytics-api.buildkite.localhost/v1/uploads"
+  token: "MJhvUk8534PkQ5nf7yfD8vHS",
+  url: "http://analytics-api.buildkite.com/v1/uploads"
 )
